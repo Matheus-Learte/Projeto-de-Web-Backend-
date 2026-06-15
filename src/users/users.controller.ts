ea70259aa16823f262b241a2af8f9da8757e988a
Dr-Verdin <piscioneri24122004@gmail.com>
2026-06-15 13:48:58 -0300

    crud userok?

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,26 @@
+import { Body, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
 
-    @Get()
-    findAll() {
-        return 'This action returns all users';
-    }
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        const res = 2 + parseInt(id);
-        return '2 + ' + id + ' é ' + res;
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findAll().then(users =>
+      users.find(u => u.id === Number(id))
+    );
+  }
+
+  @Post()
+  create(@Body() data: CreateUserDto) {
+    return this.usersService.create(data);
     }
 }
